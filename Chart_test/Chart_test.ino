035d9df412b66e5c221c34b71857c1c654e7eb86
julienrat <julienrat@riseup.net>
2016-05-17 18:54:08 +0200

    / ‘Chart_test/Chart_test.ino’ + ‘Chart_test/data/Chart.js’ + ‘Chart_test/data/canvasjs.min.js’ / ‘Chart_test/data/test.html’ + ‘Chart_test/data/test_2.html’

--- Chart_test/Chart_test.ino
+++ Chart_test/Chart_test.ino
@@ -1,86 +1,74 @@
 
 
-/* Create a WiFi access point and provide a web server on it. */
-
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include "FS.h"
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+int result=0;
 /* Set these to your desired credentials. */
-const char *ssid = "Chart_test";
+const char *ssid = "CaptoBox";
 const char *password = "1234567890";
-#define DBG_OUTPUT_PORT Serial
+
 ESP8266WebServer server(80);
 
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
- * connected to this access point to see it.
- */
-void handleRoot() {
-  server.send(200, "text/html", "<h1>You are connected</h1>");
-}
-
-//format bytes
-String formatBytes(size_t bytes) {
-  if (bytes < 1024) {
-    return String(bytes) + "B";
-  } else if (bytes < (1024 * 1024)) {
-    return String(bytes / 1024.0) + "KB";
-  } else if (bytes < (1024 * 1024 * 1024)) {
-    return String(bytes / 1024.0 / 1024.0) + "MB";
-  } else {
-    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
-  }
-}
 
 void setup() {
   delay(1000);
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH);
   Serial.begin(115200);
   Serial.println();
-  Serial.print("Configuring access point...");
+  Serial.print("Configuration en point d'acces...");
   /* You can remove the password parameter if you want the AP to be open. */
-  WiFi.softAP(ssid, password);
-
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+  WiFi.softAP(ssid);
 
   SPIFFS.begin();
-  {
-    Dir dir = SPIFFS.openDir("/");
-    while (dir.next()) {
-      String fileName = dir.fileName();
-      size_t fileSize = dir.fileSize();
-      DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
-    }
-    DBG_OUTPUT_PORT.printf("\n");
-  }
-  String str = "";
+  Dir dir = SPIFFS.openDir("/");
+
+  serveur();
+  server.begin();
+
+}
 
 
-  Serial.println("HTTP server started");
+void loop() {
+  server.handleClient();
+}
+
+void serveur() {
+
   server.on("/", HTTP_GET, []() {
     if (!handleFileRead("/index.htm")) server.send(404, "text/plain", "FileNotFound");
   });
-  server.on("/list", HTTP_GET, handleFileList);
+
+  server.on("/yolo", HTTP_GET, []() {
+   // DHT dht(DHTPIN, DHTTYPE);
+   // dht.begin();
+    result = analogRead(A0);
+    Serial.println(result);
+  });
+
+  server.on("/all", HTTP_GET, []() {
+    digitalWrite(BUILTIN_LED, LOW);
+    String json = "{";
+    json += "\"heap\":" + String(ESP.getFreeHeap());
+    json += ", \"analog\":" + String(analogRead(A0));
+    json += ", \"gpio\":" + String((uint32_t)(((GPI | GPO) & 0xFFFF) | ((GP16I & 0x01) << 16)));
+    json += "}";
+    server.send(200, "text/json", json);
+    json = String();
+    digitalWrite(BUILTIN_LED, HIGH);
+  });
+
   server.onNotFound([]() {
     if (!handleFileRead(server.uri()))
       server.send(404, "text/plain", "FileNotFound");
   });
-  
-  
-  
-   server.on("/all", HTTP_GET, [](){
-    String json = "{";
-    json += "\"heap\":"+String(ESP.getFreeHeap());
-    json += ", \"analog\":"+String(analogRead(A0));
-    json += ", \"gpio\":"+String((uint32_t)(((GPI | GPO) & 0xFFFF) | ((GP16I & 0x01) << 16)));
-    json += "}";
-    server.send(200, "text/json", json);
-    json = String();
-  });
-  server.begin();
-
 }
+
 
 
 String getContentType(String filename) {
@@ -101,7 +89,7 @@ String getContentType(String filename) {
 }
 
 bool handleFileRead(String path) {
-  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  Serial.println("handleFileRead: " + path);
   if (path.endsWith("/")) path += "index.htm";
   String contentType = getContentType(path);
   String pathWithGz = path + ".gz";
@@ -114,34 +102,4 @@ bool handleFileRead(String path) {
     return true;
   }
   return false;
-}
-void handleFileList() {
-  if (!server.hasArg("dir")) {
-    server.send(500, "text/plain", "BAD ARGS");
-    return;
-  }
-
-  String path = server.arg("dir");
-  DBG_OUTPUT_PORT.println("handleFileList: " + path);
-  Dir dir = SPIFFS.openDir(path);
-  path = String();
-
-  String output = "[";
-  while (dir.next()) {
-    File entry = dir.openFile("r");
-    if (output != "[") output += ',';
-    bool isDir = false;
-    output += "{\"type\":\"";
-    output += (isDir) ? "dir" : "file";
-    output += "\",\"name\":\"";
-    output += String(entry.name()).substring(1);
-    output += "\"}";
-    entry.close();
-  }
-
-  output += "]";
-  server.send(200, "text/json", output);
-}
-void loop() {
-  server.handleClient();
 }
